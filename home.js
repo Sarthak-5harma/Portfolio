f7c998df23ddbf4dfee45e2ac841b27daa6fed55
Sarthak-5harma <46453190+Sarthak-5harma@users.noreply.github.com>
2022-08-05 01:22:38 +0545

    Add files via upload

---
+++ home.js
@@ -0,0 +1,22 @@
+
+function gitchange(){
+    document.getElementById("git").src = "images/github_dark.png";
+}
+function gitchangepre(){
+    document.getElementById("git").src = "images/github.png"
+}
+
+function linkchange(){
+    document.getElementById("link").src = "images/linkedin_dark.png";
+}
+function linkchangepre(){
+    document.getElementById("link").src = "images/linkedin.png"
+}
+   
+function instachange(){
+    document.getElementById("insta").src = "images/instagram_dark.png";
+}
+function instachangepre(){
+    document.getElementById("insta").src = "images/instagram.png"
+}
+   
